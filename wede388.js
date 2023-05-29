deb2c0d80fcd9bc3d9b25330f637bd0b4678cdd3
CellvinWie <134899844+CellvinWie@users.noreply.github.com>
2023-05-29 13:32:55 +0700

    Add files via upload

---
+++ wede388.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://i.postimg.cc/1z2KZTkg/BANNER-RTP-1.jpg",
+      "alt": "BANNER RTP 1"
+    },
+    {
+      "url": "https://i.postimg.cc/fWxf68BL/BANNER-RTP-2.jpg",
+      "alt": "BANNER RTP 2"
+    },
+    {
+      "url": "https://i.postimg.cc/zGfwH85b/BANNER-RTP-3.jpg",
+      "alt": "BANNER RTP 3"
+    }
+  ]
+}
